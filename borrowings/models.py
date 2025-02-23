455d386fc6d8d9985ddbd674a1a0fb3c3cc6205c
Oleksandr Semenenko <semenenko233249@gmail.com>
2025-02-23 23:54:09 +0200

    Creating Borrowing model for the borrowings app

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from user.models import Customer
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowed_book")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="borrowed_customer")
+
+    class Meta:
+        ordering = ["-borrow_date"]
+        unique_together = ["borrow_date", "expected_return_date", "actual_return_date"]
+
+    def __str__(self) -> str:
+        return f"{self.book.title} - {self.borrow_date}"
--- user/urls.py
+++ user/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     # JWT token authentication
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ] + router.urls
